a0a987c6099ec98a7ca19217d40c9e49e89f5e66
Abishek1109 <53231328+Abishek1109@users.noreply.github.com>
2020-02-10 12:41:46 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,6 @@
+a=int(input('Enter the first number:'))
+b=int(input('Enter the second number: '))
+
+sum=a+b
+
+print('addition of {} and {} is {}'.format(a,b,sum))
---
+++ ex.py
@@ -0,0 +1,9 @@
+a=int(input('Enter the first number:'))
+b=int(input('Enter the second number: '))
+
+sum=a+b
+diff=a-b
+mul=a*b
+div=a/b
+
+print(' Addition: {} \n Subtraction: {} \n Multiplication: {} \n Division: {}'.format(sum,diff,mul,div))
